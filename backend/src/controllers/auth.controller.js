9f0aa2114a58d939206e5527839d38f6d432b157
maaz-shakeel0 <maazshakeel56@gmail.com>
2025-02-05 15:07:10 +0530

    Added backend function for updateProfile and to check the user is authenticated or not

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs'
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -80,9 +81,34 @@ export const login = async (req, res) => {
 export const logout = (req, res) => {
     try {
         res.cookie("jwt", "", { maxAge: 0 })
-        res.status(200).json({message:'Logout Successfull'})
+        res.status(200).json({ message: 'Logout Successfull' })
     } catch (error) {
         console.log(`Error in logout Controller ${error.message}`);
         res.status(500).json({ message: 'Logout Failed' })
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilePic } = req.body;
+        const userId = req.user._id;
+        if (!profilePic) {
+            res.status(400).json({ message: 'Profile Pic is required' })
+        }
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUuser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true })
+        res.status(200).json({ message: 'Profile updated successfully', user: updatedUuser })
+    } catch (error) {
+        console.log(`Error in updateProfile Controller ${error.message}`);
+        res.status(500).json({ message: 'Internal Server Error' })
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user)
+    } catch (error) {
+        console.log(`Error in checkAuth Controller ${error.message}`);
+        res.status(500).json({ message: 'Internal Server Error' })
     }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,14 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './lib/db.js'
 import authRoutes from './routes/auth.route.js'
-import e from 'express'
+import cookieParser from 'cookie-parser'
 const app = express()
 
 dotenv.config()
 const PORT = process.env.PORT
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
 
