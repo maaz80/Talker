5be24e8bad07ccdf9ccb27f57e5122812df7dd79
maaz-shakeel0 <maazshakeel56@gmail.com>
2025-02-05 20:49:48 +0530

    Added Messages Functions and Some frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='fixed w-full top-0 bg-white/10 rounded-b-md text-white '>
+      Maaz SHakeel
+    </div>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='h-screen flex justify-center items-center w-full'>
+      THis is home
+    </div>
+  )
+}
+
+export default Home
